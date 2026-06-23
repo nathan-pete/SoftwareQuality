b0f09ee339439ffe75b5affbdb4f943525e913a0
Nathan Pete <nathpete16@gmail.com>
2026-06-23 19:47:17 +0200

    Update JabberPoint.java
    Co-Authored-By: stefani-margaritova <91316578+stefani-margaritova@users.noreply.github.com>

--- Jabberpoint-Refactored/src/JabberPoint.java
+++ Jabberpoint-Refactored/src/JabberPoint.java
@@ -15,6 +15,7 @@ import java.io.IOException;
 
 public class JabberPoint
 {
+<<<<<<< Updated upstream
     protected static final String IOERR = "IO Error: ";
     protected static final String JABERR = "Jabberpoint Error ";
     protected static final String JABVERSION = "Jabberpoint 1.6 - OU version";
@@ -46,4 +47,37 @@ public class JabberPoint
                 JOptionPane.ERROR_MESSAGE);
         }
     }
+=======
+	protected static final String IOERR = "IO Error: ";
+	protected static final String JABERR = "Jabberpoint Error ";
+	protected static final String JABVERSION = "Jabberpoint 1.6 - OU version";
+
+	/**
+	 * Het Main Programma
+	 */
+	public static void main(String argv[])
+	{
+
+		Style.createStyles();
+		Presentation presentation = new Presentation();
+		new SlideViewerFrame(JABVERSION, presentation);
+		try
+		{
+			if (argv.length == 0)
+			{ // een demo presentatie
+				Accessor.getDemoAccessor().loadFile(presentation, "");
+			}
+			else
+			{
+				new XMLAccessor().loadFile(presentation, argv[0]);
+			}
+			presentation.setSlideNumber(0);
+		} catch (IOException ex)
+		{
+			JOptionPane.showMessageDialog(null,
+				IOERR + ex, JABERR,
+				JOptionPane.ERROR_MESSAGE);
+		}
+	}
+>>>>>>> Stashed changes
 }
