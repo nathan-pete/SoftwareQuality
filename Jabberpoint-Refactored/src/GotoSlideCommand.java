460de41ba3594e0629d219305fd06b2102b6075f
Nathan Pete <nathpete16@gmail.com>
2026-06-23 22:21:59 +0200

    Create GotoSlideCommand.java
    Prompts for a page number and jumps to that slide
    
    Co-Authored-By: stefani-margaritova <91316578+stefani-margaritova@users.noreply.github.com>

---
+++ Jabberpoint-Refactored/src/GotoSlideCommand.java
@@ -0,0 +1,21 @@
+import javax.swing.JOptionPane;
+
+public class GotoSlideCommand implements Command
+{
+
+	protected static final String PAGENR = "Page number?";
+
+	private Presentation presentation;
+
+	public GotoSlideCommand(Presentation presentation)
+	{
+		this.presentation = presentation;
+	}
+
+	public void execute()
+	{
+		String pageNumberStr = JOptionPane.showInputDialog((Object) PAGENR);
+		int pageNumber = Integer.parseInt(pageNumberStr);
+		presentation.setSlideNumber(pageNumber - 1);
+	}
+}
