848d84da6eab141c27ce0c356f7cc24ad05d951a
Nathan Pete <nathpete16@gmail.com>
2026-06-23 22:22:11 +0200

    Create AboutCommand.java
    Shows the About dialog via AboutBox.show
    
    Co-Authored-By: stefani-margaritova <91316578+stefani-margaritova@users.noreply.github.com>

---
+++ Jabberpoint-Refactored/src/AboutCommand.java
@@ -0,0 +1,19 @@
+import java.awt.Frame;
+
+// AboutCommand shows the application's About dialog.
+public class AboutCommand implements Command
+{
+
+	private Frame parent;
+
+	public AboutCommand(Frame parent)
+	{
+		this.parent = parent;
+	}
+
+	public void execute()
+	{
+		AboutBox.show(parent);
+	}
+}
+
